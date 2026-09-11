94a375603c73ee9aba1d456caecc26a2370a214f
johan <lonter02@yahoo.com>
2026-09-11 06:53:14 +0200

    Eerste versie

---
+++ info.rkt
@@ -0,0 +1,27 @@
+#lang info
+
+
+(define collection "jsonic")
+
+
+(define version "1.0")
+(define license 'Unlicense)
+
+(define scribblings '(("scribblings/jsonic.scrbl")))
+
+
+(define test-omit-paths '("jsonic-test.rkt"))
+
+
+(define deps
+ 
+             '("base"
+               "beautiful-racket-demo"
+               "beautiful-racket-lib"
+               "brag-lib"
+               "scribble-lib"
+               "scribble-text-lib"))
+(define build-deps
+     
+             '("racket-doc"))
+
